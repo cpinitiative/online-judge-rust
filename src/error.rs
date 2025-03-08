c34098ceb20908e8652371777b9554fef81e2b70
Nathan Wang <nathan.r.wang@gmail.com>
2025-03-08 03:41:14 -0500

    try to fix error message logging again

--- src/error.rs
+++ src/error.rs
@@ -10,7 +10,7 @@ pub struct AppError(anyhow::Error);
 // Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        error!(error = ?self.0, "Returning Internal Server Error");
+        error!("Returning Internal Server Error: {:?}", self.0);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
             format!("Internal Server Error: {:?}", self.0),
